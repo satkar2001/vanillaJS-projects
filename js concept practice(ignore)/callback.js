20e25f9420eaf8d7d73ffac8ad690aa00d3725e5
satkar2001 <satkaracharya883@gmail.com>
2021-08-25 23:03:07 +0530

    calls made

---
+++ js concept practice(ignore)/callback.js
@@ -0,0 +1,25 @@
+const posts=[
+    {title:'1st post' body:'this is the first one to be called'},
+    {title:'2nd post' body:'this is the second one to be called'}
+];
+
+
+function getPosts(){
+    setTimeout(()=>{
+     let output='';
+    posts.forEach((post,index)=>{
+     output+=`<li>${post}</li>`
+    });
+    document.body.innerHTML=output;
+    },1000);
+}
+
+function createPost(post,callback){
+    setTimeout(()=>{
+        posts.push(post);
+        callback();
+
+    },2000)
+}
+
+createPost({title:'3rd post' body:'this is the third one to be called'},getPosts)
