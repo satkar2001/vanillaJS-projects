501a558cced0bc13fa2e965303c46bee49246cb1
satkar2001 <satkaracharya883@gmail.com>
2021-08-27 23:39:53 +0530

    error msg added

--- js concept practice(ignore)/promises.js
+++ js concept practice(ignore)/promises.js
@@ -15,10 +15,16 @@ function getPosts(){
 }
 
 function createPost(post){
- return new Promise(resolve,reject)=>{
+ return new Promise((resolve,reject)=>{
      setTimeout(()=>{
          posts.push(post);
          const error=false;
-     })
- }
+         if(!error){
+             resolve();
+         }
+         else{
+             reject('Error: something went wrong');
+         }
+     },2000);
+ });
 }
