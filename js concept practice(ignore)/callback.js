b0c4fd55d7ef33c0b38f685d28ef64b8b694d5ca
satkar2001 <satkaracharya883@gmail.com>
2021-08-27 23:29:16 +0530

    new html file

--- js concept practice(ignore)/callback.js
+++ js concept practice(ignore)/callback.js
@@ -1,6 +1,6 @@
 const posts=[
-    {title:'1st post' body:'this is the first one to be called'},
-    {title:'2nd post' body:'this is the second one to be called'}
+    {title:'1st post' ,body:'this is the first one to be called'},
+    {title:'2nd post', body:'this is the second one to be called'}
 ];
 
 
@@ -8,7 +8,7 @@ function getPosts(){
     setTimeout(()=>{
      let output='';
     posts.forEach((post,index)=>{
-     output+=`<li>${post}</li>`
+     output+=`<li>${post.title}</li>`
     });
     document.body.innerHTML=output;
     },1000);
@@ -22,4 +22,4 @@ function createPost(post,callback){
     },2000)
 }
 
-createPost({title:'3rd post' body:'this is the third one to be called'},getPosts)
+createPost({title:'3rd post', body:'this is the third one to be called'},getPosts)
