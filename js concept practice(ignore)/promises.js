f02b5528c52748eb377eb90c85e8df96a445049d
satkar2001 <satkaracharya883@gmail.com>
2021-09-04 23:41:36 +0530

    made a new method for promise

--- js concept practice(ignore)/promises.js
+++ js concept practice(ignore)/promises.js
@@ -29,6 +29,8 @@ function createPost(post){
  });
 }
 
-createPost({title:'post three',body:'this is post three'})
- .then(getPosts)
- .catch(err=>console.log(err));
+// createPost({title:'post three',body:'this is post three'})
+//  .then(getPosts)
+//  .catch(err=>console.log(err));
+
+const promise1=promise.resolve('hello world');
