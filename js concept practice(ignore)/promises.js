411e69446daa0b58d20d48c9efd7b07c2850b1bb
satkar2001 <satkaracharya883@gmail.com>
2021-09-04 23:47:26 +0530

    api request

--- js concept practice(ignore)/promises.js
+++ js concept practice(ignore)/promises.js
@@ -37,5 +37,9 @@ const promise1=promise.resolve('hello world');
 const promise2=10;
 const promise3=new Promise((resolve,reject)=>setTimeout(resolve,2000,'goodbye'));
 
-const
+const promise4=fetch("https://jsonplaceholder.typicode.com/users")
+.then(res=>res.json());
+
+promise.all((promise1,promise2,promise3,promise4))
+.then(values=>console.log(values));
 
